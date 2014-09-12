d394ba82f651a04a99cef6a8142dd9d6be93182f
PeoneEr <peoneerko@yandex.ru>
2014-09-12 13:29:51 +0700

    autocommit

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -3,4 +3,4 @@ role :web, %w{deploy@31.211.41.20}
 role :db,  %w{deploy@31.211.41.20}
 
 set :stage, :production
-server '192.168.0.105', user: 'deploy', roles: %w{web app db}
+server '31.211.41.20' user: 'deploy', roles: %w{web app db}
