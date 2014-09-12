6ade4a6b06a3bf7634f981f3f35d9ec085d8d914
PeoneEr <peoneerko@yandex.ru>
2014-09-12 13:30:31 +0700

    typo fix

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -3,4 +3,4 @@ role :web, %w{deploy@31.211.41.20}
 role :db,  %w{deploy@31.211.41.20}
 
 set :stage, :production
-server '31.211.41.20' user: 'deploy', roles: %w{web app db}
+server '31.211.41.20', user: 'deploy', roles: %w{web app db}
