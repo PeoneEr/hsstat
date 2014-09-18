be1957b8a3db7c5f01225548dfae3278cb1a9471
PeoneEr <peoneerko@yandex.ru>
2014-09-18 20:31:10 +0700

    menu on top

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,9 @@
 module ApplicationHelper
+  def percentages(win, all, precision = 1)
+    number_to_percentage((win / all) * 100, precision: precision)
+  end
+
+  def percentages_for_stat(stat, precision = 1)
+    number_to_percentage(stat, precision: precision)
+  end
 end
